f3398b2a7803aebe10c9b07dbddc6864198fae53
MisterScrooge <shawnhumbert13@gmail.com>
2022-09-17 21:45:38 +0800

    done 1,2

--- activity1.rb
+++ activity1.rb
@@ -1,5 +1,16 @@
-puts "Hello World"
-puts "Hello Aliens"
-puts "Hello Batch21"
-puts "Hello its me"
-puts "Hello farewell"
+# solution for 1
+puts "solution for 1"
+numbers = [1,2,3,4,5,6,7,8,9,10]
+numbers.each do |n|
+    puts n
+end
+
+# solution for 2
+puts "solution for 2"
+h={a:1, b:2, c:3, d:4}
+puts h[:b]
+h[:e] = 5
+puts h
+
+# solution for 3
+puts "solution for 3"
